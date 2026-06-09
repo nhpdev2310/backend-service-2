c0e2c7ef72ed2083ec29b7ebbc408cd5ee358a30
nhpdev2310 <nhpdev1995@gmail.com>
2026-06-09 16:34:59 +0700

    test: add test profile and mock env vars to fix CI context load

--- src/test/java/com/nhpdev/backendservicesecond/BackendServiceSecondApplicationTests.java
+++ src/test/java/com/nhpdev/backendservicesecond/BackendServiceSecondApplicationTests.java
@@ -2,8 +2,10 @@ package com.nhpdev.backendservicesecond;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendServiceSecondApplicationTests {
 
     @Test
